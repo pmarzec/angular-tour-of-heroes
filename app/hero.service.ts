3e94155d273fc60ff6bc5c84f2b712d020ef92be
Piotr Marzec <pmarzec@clear2pay.com>
2016-10-17 11:58:46 +0200

    Async Services and Promises.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,8 +4,14 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Hero[] {
-        return HEROES;
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+    // for testing purpose.
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)) // delay 2 seconds
+            .then(() => this.getHeroes());
     }
 }
 
